25152a1388b3954ee54caaa12f2b3b9153f38fea
MarcosAmaral9 <89167718+MarcosAmaral9@users.noreply.github.com>
2026-01-24 18:14:00 -0300

    Update ACValhalla.tsx

--- src/pages/posts/ACValhalla.tsx
+++ src/pages/posts/ACValhalla.tsx
@@ -16,6 +16,16 @@ const ACValhalla = () => {
         <ArrowLeft className="h-4 w-4" />
         Voltar para Portal Assassin's Creed
       </Link>
+
+      {/* Ranking Button */}
+      <article className="container w-full flex justify-center pt-8 pb-6">
+      <Link
+    to="/geek/ranking-melhor-assassins-creed-2026"
+    className="inline-flex items-center gap-2 text-muted-foreground hover:text-foreground transition-colors"
+      >
+    <ArrowLeft className="h-4 w-4" />
+    Voltar para o Ranking
+  </Link>
       
       {/* Header */}
       <header className="mb-8">
